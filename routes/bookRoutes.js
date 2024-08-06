e9f93c584034f1948e0dc9eb88db4aab22121c1f
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-08-06 14:52:29 +0530

    kushan

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -9,5 +9,6 @@ router.post('/books', bookController.addBook);
 //
 router.get('/getBookId/:ISBN', bookController.getBookIdWithISBN);
 router.put('/getBookId', getBookIdController.getBookIdController);
+router.get('/getBookWithBookId/:bookId', bookController.getBookIdController);
 
 module.exports = router;
