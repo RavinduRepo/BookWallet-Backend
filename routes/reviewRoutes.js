1233b422ac26d06d425bf0b9d639394dc57d069d
Akash <e20100@eng.pdn.ac.lk>
2024-08-14 16:45:15 +0530

    fetch comments connectiom created

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,21 +6,26 @@ const {
   getReviewWithBookId,
   getReviewWithUserId,
 } = require("../services/reviewService");
+
 // const { getLikesIdByReviewId } = require("../services/reviewLikesService");
 const { ReviewPost } = require("../controllers/reviewPostController");
+
 router.post("/reviewpost", ReviewPost); //Not in use
 router.get("/getReviewWithId/:reviewId", getReviewWithId);
 router.get("/getReviews", getReviews);
 router.get("/getReviewWithBookId/:bookId", getReviewWithBookId);
 router.get("/getReviewWithUserId/:userId", getReviewWithUserId);
 // router.get("/:reviewId/likes", getLikesIdByReviewId);
-const reviewLikesController = require("../controllers/reviewLikesController"); // Ensure this path is correct
+const reviewLikesController = require("../controllers/reviewLikesController");
 
-const {
-  addCommentController,
-} = require("../controllers/reviewCommentsController");
+const reviewcommentsController = require("../controllers/reviewCommentsController");
 // Route to add a comment
-router.post("/comments/add", addCommentController);
+router.post("/comments/add", reviewcommentsController.addCommentController);
+// Define the route to get comments by review ID
+router.get(
+  "/:reviewId/comments",
+  reviewcommentsController.getCommentsByReviewIdController
+);
 
 // Route to get users who liked a review
 router.get("/:reviewId/likes", reviewLikesController.getUsersWhoLikedReview); // Ensure this function exists
