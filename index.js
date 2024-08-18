841a7d4bfda0d1019ad93fde56828a9baa350a43
RaviAlpha <ravindupathirage12345@gmail.com>
2024-08-18 19:38:33 +0530

    added mssql

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get('/get-users', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
