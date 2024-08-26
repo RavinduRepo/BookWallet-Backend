f33113c9613fbec0c4c1adcd5080ffb68192f652
RaviAlpha <ravindupathirage12345@gmail.com>
2024-08-26 21:47:21 +0530

    tested google db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,26 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+// const fs = require('fs');
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Create a pool of connections to the database
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,          // Hostname of the database server
+  user: process.env.DB_USER,          // Database username
+  password: process.env.DB_PASSWORD,  // Database password
+  database: process.env.DB_NAME,      // Name of the database to use
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
+  // ssl: {
+  //   // SSL certificates for secure connection
+  //   ca: fs.readFileSync('./server-ca.pem'),   // Replace with actual path to server CA file
+  //   key: fs.readFileSync('./client-key.pem'), // Replace with actual path to client key file
+  //   cert: fs.readFileSync('./client-cert.pem') // Replace with actual path to client cert file
+  // },
 });
 
-// console.log('DB_HOST:', process.env.DB_HOST);
-// console.log('DB_USER:', process.env.DB_USER);
-// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-// console.log('DB_NAME:', process.env.DB_NAME);
-
+// Export the pool to use in other parts of the application
 module.exports = pool.promise();
