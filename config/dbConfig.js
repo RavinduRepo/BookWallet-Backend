658033100a29ea2a1e41d4536e594c1f047ab787
RaviAlpha <ravindupathirage12345@gmail.com>
2024-09-14 17:13:53 +0530

    test hosting  removed ssl

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-const fs = require('fs');
+// const fs = require('fs');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -11,11 +11,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,          // Database username
   password: process.env.DB_PASSWORD,  // Database password
   database: process.env.DB_NAME,      // Name of the database to use
-  ssl: {
-    key: process.env.DB_SSL_KEY,
-    cert: process.env.DB_SSL_CERT,
-    ca: process.env.DB_SSL_CA
-  }
+  // ssl: {
+  //   key: process.env.DB_SSL_KEY,
+  //   cert: process.env.DB_SSL_CERT,
+  //   ca: process.env.DB_SSL_CA
+  // }
 });
 
 // Export the pool to use in other parts of the application
