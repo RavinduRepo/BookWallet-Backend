7533a58f313b0875e77a4b35aa3da4ee1683e486
Akash <e20100@eng.pdn.ac.lk>
2024-09-24 19:11:03 +0530

    saved

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config()  ;
+require("dotenv").config();
 const db = require("./config/dbConfig");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -20,11 +20,12 @@ const notificationRoutes = require("./routes/notificationRoutes");
 const storeRoutes = require("./store_management/storeRoutes");
 const savedItemsRoutes = require("./routes/savedItemsRoutes");
 const trendingRoutes = require("./routes/trendingRoutes");
+const bookStatusCheckingRoutes = require("./routes/bookStatusCheckingRoutes"); //book wishlist and save status
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 require("./updatetrending");
- 
+
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
@@ -43,6 +44,7 @@ app.use("/api/notification", notificationRoutes);
 app.use("/api/stores", storeRoutes);
 app.use("/api/saved-items", savedItemsRoutes);
 app.use("/api/trending", trendingRoutes);
+app.use("/api/books", bookStatusCheckingRoutes); //book wishlist and save status
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const wishlistController = require('../controllers/wishlistController');
-const wishlistservice = require('../services/wishlistService');
-const isInWishlistController = require('../controllers/isInWishlistController');
+const wishlistController = require("../controllers/wishlistController");
+const wishlistservice = require("../services/wishlistService");
 
-router.get('/IsinWishlist/:userId/:bookId', isInWishlistController.checkWishlist);
-router.get('/:userId', wishlistController.getWishlistByUserId);
-router.post('/wishlistBooks/:bookId/:userId',wishlistservice.postWishlistBook);
-router.delete('/remove/:userId/:bookId', wishlistController.removeFromWishlist);
-router.put('/wishlistgetId', wishlistController.getBookIdforwishlist);
+router.get("/:userId", wishlistController.getWishlistByUserId);
+router.post("/wishlistBooks/:bookId/:userId", wishlistservice.postWishlistBook);
+router.delete("/remove/:userId/:bookId", wishlistController.removeFromWishlist);
+router.put("/wishlistgetId", wishlistController.getBookIdforwishlist);
 
 module.exports = router;
