056c20ea058232993cde650a0198666e0a7ae6a4
Akash <e20100@eng.pdn.ac.lk>
2024-10-11 14:40:06 +0530

    Documentation and unit test added

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,118 +1,174 @@
-const reviewService = require('../services/reviewService');
-const authService = require('../services/authService');
+const reviewService = require("../services/reviewService");
+const authService = require("../services/authService");
 
+// Fetch a specific review by its ID
 const getReviewWithId = async (req, res) => {
   try {
-    const { reviewId } = req.params;
+    const { reviewId } = req.params; // Extract review ID from the request URL
 
+    // Check if the review ID is provided
     if (!reviewId) {
       return res.status(400).json({ message: "Review ID is required" });
     }
 
+    // Fetch the review details from the review service
     const reviewDetail = await reviewService.getReviewWithId(reviewId);
 
+    // If no review is found, return a 404 error
     if (!reviewDetail) {
       return res.status(404).json({ message: "Review not found" });
     }
 
+    // Send the review details in the response
     res.status(200).json(reviewDetail);
   } catch (error) {
     console.error("Error fetching review details:", error);
-    res.status(500).json({ message: "Server error while fetching review details" });
+    // Handle server error during review retrieval
+    res
+      .status(500)
+      .json({ message: "Server error while fetching review details" });
   }
 };
 
+// Fetch all reviews
 const getReviews = async (req, res) => {
   try {
+    // Fetch all reviews from the review service
     const reviewDetails = await reviewService.getReviews();
 
+    // If no reviews are found, return a 404 error
     if (reviewDetails.length === 0) {
       return res.status(404).json({ message: "No reviews found" });
     }
 
+    // Send the list of reviews in the response
     res.status(200).json(reviewDetails);
   } catch (error) {
     console.error("Error fetching review details:", error);
-    res.status(500).json({ message: "Server error while fetching review details" });
+    // Handle server error during reviews retrieval
+    res
+      .status(500)
+      .json({ message: "Server error while fetching review details" });
   }
 };
 
+// Fetch all reviews associated with a specific book
 const getReviewWithBookId = async (req, res) => {
   try {
-    const { bookId } = req.params;
+    const { bookId } = req.params; // Extract book ID from the request URL
 
+    // Check if the book ID is provided
     if (!bookId) {
       return res.status(400).json({ message: "Book ID is required" });
     }
 
+    // Fetch reviews related to the specified book from the review service
     const posts = await reviewService.getReviewWithBookId(bookId);
 
+    // If no reviews are found, return a 404 error
     if (posts.length === 0) {
       return res.status(404).json({ message: "Review not found" });
     }
 
+    // Send the reviews in the response
     res.status(200).json(posts);
   } catch (error) {
     console.error("Error fetching review details:", error);
-    res.status(500).json({ message: "Server error while fetching review details" });
+    // Handle server error during review retrieval
+    res
+      .status(500)
+      .json({ message: "Server error while fetching review details" });
   }
 };
 
+// Fetch all reviews posted by a specific user
 const getReviewWithUserId = async (req, res) => {
   try {
-    const { userId } = req.params;
+    const { userId } = req.params; // Extract user ID from the request URL
 
+    // Check if the user ID is provided
     if (!userId) {
       return res.status(400).json({ message: "User ID is required" });
     }
 
+    // Fetch reviews posted by the user from the review service
     const posts = await reviewService.getReviewWithUserId(userId);
 
+    // If no reviews are found, return a 404 error
     if (posts.length === 0) {
       return res.status(404).json({ message: "Review not found" });
     }
 
+    // Send the reviews in the response
     res.status(200).json(posts);
   } catch (error) {
     console.error("Error fetching review details:", error);
-    res.status(500).json({ message: "Server error while fetching review details" });
+    // Handle server error during review retrieval
+    res
+      .status(500)
+      .json({ message: "Server error while fetching review details" });
   }
 };
 
+// Delete a specific review
 const deleteReview = async (req, res) => {
-  const { reviewId, userId } = req.params;
-  const { token } = req.body;
+  const { reviewId, userId } = req.params; // Extract review and user ID from the request URL
+  const { token } = req.body; // Extract token from the request body
 
   try {
+    // Verify the token and retrieve the user ID from the token
     const decoded = await authService.verifyToken(token);
     const userIdToken = decoded.id.toString();
+
+    // Check if the user from the token matches the user attempting to delete the review
     if (userIdToken !== userId) {
-        return res.status(403).json({ error: 'Unauthorized action' });
+      return res.status(403).json({ error: "Unauthorized action" });
     }
+
+    // Call review service to delete the review
     await reviewService.deleteReview(reviewId, userId);
-    return res.status(200).json({ message: 'Review deleted successfully.' });
+
+    // Send success response if the review is deleted successfully
+    return res.status(200).json({ message: "Review deleted successfully." });
   } catch (error) {
-    console.error('Error deleting review:', error);
-    return res.status(500).json({ message: 'Failed to delete review.' });
+    console.error("Error deleting review:", error);
+    // Handle server error during review deletion
+    return res.status(500).json({ message: "Failed to delete review." });
   }
 };
 
+// Update a specific review
 const updateReview = async (req, res) => {
-    const { reviewId, userId } = req.params;
-    const { content, rating, token } = req.body;
+  const { reviewId, userId } = req.params; // Extract review and user ID from the request URL
+  const { content, rating, token } = req.body; // Extract review content, rating, and token from the request body
 
-    try {
-        const decoded = await authService.verifyToken(token);
-        const userIdToken = decoded.id.toString();
-        if (userIdToken !== userId) {
-            return res.status(403).json({ error: 'Unauthorized action' });
-        }
-        await reviewService.updateReview(reviewId, userId, content, rating);
-        return res.status(200).json({error: 'Review updated successfully'});
-    } catch (error) {
-        console.error('Error updating review:', error);
-        return res.status(500).json({message: 'Failed to update review'});
+  try {
+    // Verify the token and retrieve the user ID from the token
+    const decoded = await authService.verifyToken(token);
+    const userIdToken = decoded.id.toString();
+
+    // Check if the user from the token matches the user attempting to update the review
+    if (userIdToken !== userId) {
+      return res.status(403).json({ error: "Unauthorized action" });
     }
-}
 
-module.exports = {getReviewWithId, getReviews, getReviewWithBookId, getReviewWithUserId, deleteReview, updateReview};
+    // Call review service to update the review with the new content and rating
+    await reviewService.updateReview(reviewId, userId, content, rating);
+
+    // Send success response if the review is updated successfully
+    return res.status(200).json({ error: "Review updated successfully" });
+  } catch (error) {
+    console.error("Error updating review:", error);
+    // Handle server error during review update
+    return res.status(500).json({ message: "Failed to update review" });
+  }
+};
+
+module.exports = {
+  getReviewWithId,
+  getReviews,
+  getReviewWithBookId,
+  getReviewWithUserId,
+  deleteReview,
+  updateReview,
+};
--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -3,10 +3,354 @@ const historyController = require("../controllers/historyController");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: History
+ *   description: API endpoints to manage History
+ */
+/**
+ * @swagger
+ * /api/history/{userId}/reviews:
+ *    get:
+ *      summary: Get reviews by user ID
+ *      tags: [History]
+ *      parameters:
+ *        - in: path
+ *          name: userId
+ *          required: true
+ *          description: The ID of the user whose reviews are to be fetched
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: Successfully fetched reviews for the user
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                    searchIndex:
+ *                      type: integer
+ *                      description: The index of the review in the search results
+ *                    post:
+ *                      type: object
+ *                      description: The review object
+ *                      properties:
+ *                        reviewId:
+ *                          type: string
+ *                          description: The ID of the review
+ *                        content:
+ *                          type: string
+ *                          description: The content of the review
+ *                        rating:
+ *                          type: integer
+ *                          description: The rating given in the review
+ *                        date:
+ *                          type: string
+ *                          format: date-time
+ *                          description: The date the review was created
+ *        500:
+ *          description: An error occurred while fetching reviews
+ */
+
 router.get("/:userId/reviews", historyController.getReviewsByUserId);
+/**
+ * @swagger
+ * /api/history/{userId}/books:
+ *   get:
+ *     summary: Get books by user ID
+ *     tags: [History]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: The ID of the user whose books are to be fetched
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully fetched books for the user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ *       400:
+ *         description: Invalid user ID or other client-side error
+ *       500:
+ *         description: Failed to retrieve books due to a server error
+ */
+
 router.get("/:userId/books", historyController.getBooksByUserId);
+/**
+ * @swagger
+ * /api/history/{userId}/user-details:
+ *   get:
+ *     summary: Get user details by user ID
+ *     tags: [History]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: The ID of the user whose details are to be fetched
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully fetched user details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userId:
+ *                   type: string
+ *                   description: The unique identifier for the user
+ *                 username:
+ *                   type: string
+ *                   description: The user's username
+ *                 email:
+ *                   type: string
+ *                   description: The user's email address
+ *                 bio:
+ *                   type: string
+ *                   description: A short biography of the user
+ *                 location:
+ *                   type: string
+ *                   description: The user's location
+ *                 joinedDate:
+ *                   type: string
+ *                   format: date
+ *                   description: The date the user joined
+ *               example:
+ *                 userId: "123"
+ *                 username: "john_doe"
+ *                 email: "john.doe@example.com"
+ *                 bio: "Avid reader and book lover"
+ *                 location: "New York, USA"
+ *                 joinedDate: "2022-05-10"
+ *       400:
+ *         description: Invalid user ID or other client-side error
+ *       500:
+ *         description: Failed to retrieve user details due to a server error
+ */
 router.get("/:userId/user-details", historyController.getUserDetailsByUserId);
+/**
+ * @swagger
+ * /api/history/all/{userId}:
+ *   get:
+ *     summary: Retrieve all items (books, reviews, etc.) associated with a user
+ *     description: Fetch all books, reviews, and any shared items associated with the specified user.
+ *     tags: [History]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: The ID of the user whose items are to be retrieved
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully fetched all items for the specified user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 oneOf:
+ *                   - $ref: '#/components/schemas/Book'
+ *                   - $ref: '#/components/schemas/Review'
+ *                   - $ref: '#/components/schemas/Share'
+ *               example:
+ *                 - bookId: 12
+ *                   title: "The Winds of Dune"
+ *                   ISBN10: "184737848X"
+ *                   ISBN13: "9781847378484"
+ *                   publication_date: "2009-10-01"
+ *                   description: "Between the end of Frank Herbert's DUNE and his next book..."
+ *                   author: "Kevin J. Anderson, Brian Herbert"
+ *                   rating: 4.5
+ *                   pages: 592
+ *                   genre: Fiction
+ *                   imageUrl: "http://books.google.com/books/content?id=BpgkiOdga..."
+ *                   resource: "Google"
+ *                 - reviewId: 10
+ *                   bookId: 116
+ *                   userId: 27
+ *                   imageUrl: ""
+ *                   title: ""
+ *                   author: ""
+ *                   context: "Excellent book with great insights!"
+ *                   rating: 4.5
+ *                   date: "2024-09-14"
+ *                   username: ""
+ *                   likesCount: 0
+ *                   commentsCount: 0
+ *                   sharesCount: 0
+ *                 - reviewId: 15
+ *                   sharerUsername: "john_doe"
+ *                   bookId: 116
+ *                   userId: 27
+ *                   imageUrl: ""
+ *                   title: ""
+ *                   author: ""
+ *                   context: "A fascinating read!"
+ *                   rating: 4.5
+ *                   date: "2024-09-14"
+ *                   username: "john_doe"
+ *                   likesCount: 0
+ *                   commentsCount: 0
+ *                   sharesCount: 0
+ *                   sharerUserId: 102
+ *       400:
+ *         description: Invalid user ID or client-side error
+ *       500:
+ *         description: Failed to retrieve items due to server error
+ */
 router.get("/all/:userId", historyController.getAllItems);
+
+/**
+ * @swagger
+ * /api/history/reviewinsert:
+ *   post:
+ *     summary: Insert review history for a user
+ *     description: Logs a review history entry for a specified user based on the token and relevant review ID.
+ *     tags: [History]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *               - relevant_id
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: JWT token of the authenticated user
+ *               relevant_id:
+ *                 type: integer
+ *                 description: The ID of the review being inserted into the history
+ *             example:
+ *               token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *               relevant_id: 35
+ *     responses:
+ *       200:
+ *         description: Review History inserted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Review History inserted successfully"
+ *       400:
+ *         description: Invalid request parameters
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid request data"
+ *       401:
+ *         description: Unauthorized request due to invalid or missing token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized: Invalid or missing token"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An error occurred while inserting review history"
+ */
+
 router.post("/reviewinsert", historyController.insertReviewHistory);
+/**
+ * @swagger
+ * /api/history/bookinsert:
+ *   post:
+ *     summary: Insert book history for a user
+ *     description: Logs a book history entry for a specified user based on the token and relevant book ID. It also increments the trending points for the book.
+ *     tags: [History]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *               - relevant_id
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: JWT token of the authenticated user
+ *               relevant_id:
+ *                 type: integer
+ *                 description: The ID of the book being inserted into the history
+ *             example:
+ *               token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *               relevant_id: 123
+ *     responses:
+ *       200:
+ *         description: Book History inserted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Book History inserted successfully"
+ *       400:
+ *         description: Invalid request parameters
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid request data"
+ *       401:
+ *         description: Unauthorized request due to invalid or missing token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized: Invalid or missing token"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "An error occurred while inserting book history"
+ */
+
 router.post("/bookinsert", historyController.insertBookHistory);
 module.exports = router;
