e5fcf8d6eab6da8bac612894d976b39ada78db95
RaviAlpha <ravindupathirage12345@gmail.com>
2024-10-12 15:24:39 +0530

    remoed jest from package.json for szure testing

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,13 +12,13 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD, // Database password
   database: process.env.DB_NAME, // Name of the database to use
 
-  // Un-comment to connect to Google cloud SQL database ....................
-  ssl: { 
-    key: fs.readFileSync(process.env.DB_SSL_KEY),
-    cert: fs.readFileSync(process.env.DB_SSL_CERT),
-    ca: fs.readFileSync(process.env.DB_SSL_CA)
-  }
-  // ....................................................................... 
+  // // Un-comment to connect to Google cloud SQL database ....................
+  // ssl: { 
+  //   key: fs.readFileSync(process.env.DB_SSL_KEY),
+  //   cert: fs.readFileSync(process.env.DB_SSL_CERT),
+  //   ca: fs.readFileSync(process.env.DB_SSL_CA)
+  // }
+  // // .......................................................................
 });
 
 // Export the pool to use in other parts of the application
